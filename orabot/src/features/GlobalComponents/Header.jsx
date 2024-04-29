39890c7bca5f2acc03c5833e943418ec7ae4b090
Jesus Enrique Diaz <a00227255@tec.mx>
2024-04-28 21:02:59 -0600

    Added back icon

--- orabot/src/features/GlobalComponents/Header.jsx
+++ orabot/src/features/GlobalComponents/Header.jsx
@@ -16,7 +16,7 @@ export const Header = (props) => {
 
     return (
         <div className="containerHeader">
-            {props.back? <img className="back" onClick={goBack} src="https://firebasestorage.googleapis.com/v0/b/oracle-java-bot.appspot.com/o/Assets%2FIcons%2FOracle_Logo.png?alt=media&token=6c11678e-d00f-4973-86c5-22851047f6ea" alt="Go Back Button"></img> : <></>} 
+            {props.back? <img className="back" onClick={goBack} src="https://firebasestorage.googleapis.com/v0/b/oracle-java-bot.appspot.com/o/Assets%2FIcons%2Fback_icon.png?alt=media&token=fbfeae8f-ec22-4de4-bf6b-5a22b9400110" alt="Go Back Button" width={20} height={20}></img> : <></>} 
             <h2>OraBot</h2>
             <img src="https://firebasestorage.googleapis.com/v0/b/oracle-java-bot.appspot.com/o/Assets%2FIcons%2FOracle_Logo.png?alt=media&token=6c11678e-d00f-4973-86c5-22851047f6ea" alt="OraBot Logo" width={40} height={20} />
         </div>
