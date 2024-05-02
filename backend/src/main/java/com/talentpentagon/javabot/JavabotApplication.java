7ff6b569d9cffdc00efb4eada695c24df16b58c6
Moisés Hiram Pineda Campos <moypineda10@gmail.com>
2024-05-01 18:04:05 -0600

    Creation of the EmailSenderService and TicketEmailController

--- backend/src/main/java/com/talentpentagon/javabot/JavabotApplication.java
+++ backend/src/main/java/com/talentpentagon/javabot/JavabotApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavabotApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(JavabotApplication.class, args);
 	}
 
---
+++ backend/src/main/java/com/talentpentagon/javabot/controller/TicketEmailController.java
@@ -0,0 +1,28 @@
+package com.talentpentagon.javabot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.mail.MessagingException;
+import com.talentpentagon.javabot.service.EmailSenderService;
+
+@RestController
+public class TicketEmailController {
+
+    @Autowired
+    private EmailSenderService senderService;
+
+    @PostMapping("/sendEmail")
+    public ResponseEntity<String> sendEmail(@RequestParam String userEmail) throws MessagingException {
+        try {
+            senderService.sendEmail(userEmail);
+            return ResponseEntity.ok("Correo enviado exitosamente a " + userEmail);
+        } catch (javax.mail.MessagingException e) {
+            return ResponseEntity.status(500).body("Error al enviar el correo: " + e.getMessage());
+        }
+    }
+}
