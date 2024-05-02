b7d3297bb3ba8ec4bbd44117aaf99da22e63f04c
Geek-Deer <geekdeer@gmail.com>
2024-05-02 10:33:17 -0600

    Solved merge conflicts

--- backend/src/main/java/com/talentpentagon/javabot/controller/TicketEmailController.java
+++ backend/src/main/java/com/talentpentagon/javabot/controller/TicketEmailController.java
@@ -1,8 +1,7 @@
 package com.talentpentagon.javabot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
