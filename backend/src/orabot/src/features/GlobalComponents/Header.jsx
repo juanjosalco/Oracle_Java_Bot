d2e03f449617a9630a26069b1239b029be1f1585
Moisés Hiram Pineda Campos <moypineda10@gmail.com>
2024-05-13 19:53:30 -0600

    Add Logout Botton
    Why do we have log in function but we don't have a log out botton?

--- backend/src/orabot/src/features/GlobalComponents/Header.jsx
+++ backend/src/orabot/src/features/GlobalComponents/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 // Styles
 import "./Styles/Header.css";
@@ -9,16 +9,42 @@ import "./Styles/Header.css";
 export const Header = (props) => {
 
     const navigate = useNavigate();
+    const location = useLocation();
+    const showLogoutButton = location.pathname === "/dashboard";
 
     const goBack = () => {
         navigate(-1);
     };
 
+    const handleLogout = () => {
+        navigate("/");
+    };
+
     return (
         <div className="containerHeader">
-            {props.back? <img className="back" onClick={goBack} src="https://firebasestorage.googleapis.com/v0/b/oracle-java-bot.appspot.com/o/Assets%2FIcons%2Fback_icon.png?alt=media&token=fbfeae8f-ec22-4de4-bf6b-5a22b9400110" alt="Go Back Button" width={20} height={20}></img> : <></>} 
+            {props.back ? <img 
+                className="back" 
+                onClick={goBack} 
+                src="https://firebasestorage.googleapis.com/v0/b/oracle-java-bot.appspot.com/o/Assets%2FIcons%2Fback_icon.png?alt=media&token=fbfeae8f-ec22-4de4-bf6b-5a22b9400110" 
+                alt="Go Back Button" 
+                width={20} height={20}
+            ></img> : <></>} 
             <h2>OraBot</h2>
-            <img src="https://firebasestorage.googleapis.com/v0/b/oracle-java-bot.appspot.com/o/Assets%2FIcons%2FOracle_Logo.png?alt=media&token=6c11678e-d00f-4973-86c5-22851047f6ea" alt="OraBot Logo" width={40} height={20} />
+            <img 
+                src="https://firebasestorage.googleapis.com/v0/b/oracle-java-bot.appspot.com/o/Assets%2FIcons%2FOracle_Logo.png?alt=media&token=6c11678e-d00f-4973-86c5-22851047f6ea" 
+                alt="OraBot Logo"
+                className="OraBot"
+                width={40} height={20} />
+
+            {showLogoutButton && (
+                <img 
+                    className="logout"
+                    onClick={handleLogout}
+                    src="https://firebasestorage.googleapis.com/v0/b/oracle-java-bot.appspot.com/o/Assets%2FIcons%2Flogout.png?alt=media&token=7c239655-25b8-4cf2-b2e3-b09e75f2a56e" 
+                    alt="Log Out" 
+                    width={20} height={20}
+                />
+            )}
         </div>
     );
 }
