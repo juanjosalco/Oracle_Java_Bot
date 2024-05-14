95cd27014e7e3be09e348446ef67bc3f623afc76
Geek-Deer <geekdeer@gmail.com>
2024-05-14 17:50:00 -0600

    Added a function to unset the previous attempts

---
+++ unset.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Unset environment variables
+unset TALENT_PENTAGON_LOCATION
+unset JAVA_HOME
+unset TALENT_PENTAGON_STATE_HOME
+unset TALENT_PENTAGON_LOG
+
+# Unset aliases
+unalias k
+unalias kt
+unalias pods
+unalias services
+unalias gateways
+unalias secrets
+unalias ingresssecret
+unalias virtualservices
+unalias deployments
+unalias talent_pentagon
