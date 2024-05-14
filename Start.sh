0d8df416e6b48a4e2ef1d55ddc1014012e7c4fd7
Geek-Deer <geekdeer@gmail.com>
2024-05-14 17:50:00 -0600

    Added a function to unset the previous attempts

--- Start.sh
+++ Start.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# First steps
 chmod +x *.sh
 cd utils
 chmod +x *.sh
 cd ..
+
+# Delete previous .env if exists
+source unset.sh
+
+# First steps
+
 echo source $(pwd)/env.sh >> ~/.bashrc
 source env.sh
 source setup.sh
+echo "Oracle Java Bot is ready to build and deploy! EXCELSIOR!"
