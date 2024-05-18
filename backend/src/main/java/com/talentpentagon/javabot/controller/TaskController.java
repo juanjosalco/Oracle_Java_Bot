05ca2a18d497ffc17cba18b715c97da02987bb28
Geek-Deer <geekdeer@gmail.com>
2024-05-17 23:35:09 -0600

    Added "/api/v1/" prefix to all endpoints
    Better practices :)

--- backend/src/main/java/com/talentpentagon/javabot/controller/TaskController.java
+++ backend/src/main/java/com/talentpentagon/javabot/controller/TaskController.java
@@ -30,8 +30,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @RestController
+@RequestMapping("/api/v1/")
 public class TaskController {
 
     @Autowired
