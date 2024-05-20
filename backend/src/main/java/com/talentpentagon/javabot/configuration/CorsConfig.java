8dc45b5726e893971b3f33218a60b7911a2685e4
Geek-Deer <geekdeer@gmail.com>
2024-05-20 12:09:08 -0600

    Added the true ip to the API config

--- backend/src/main/java/com/talentpentagon/javabot/configuration/CorsConfig.java
+++ backend/src/main/java/com/talentpentagon/javabot/configuration/CorsConfig.java
@@ -7,25 +7,26 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-
 import java.util.Collections;
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
-    Logger logger = LoggerFactory.getLogger(CorsConfig.class);
-    public CorsFilter corsFilter(){
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:8080", "http://159.54.139.5:81", "http://159.54.139.5:80", "https://159.54.139.5:81", "https://159.54.139.5:80"));
-        config.setAllowedMethods(List.of("GET","POST","PUT","OPTIONS","DELETE","PATCH"));
-        config.setAllowedOrigins(Collections.singletonList("*"));
-        config.addAllowedHeader("*");
-        config.addExposedHeader("location");
-        config.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        CorsFilter filter = new CorsFilter(source);
-        return filter;
-    }
+        Logger logger = LoggerFactory.getLogger(CorsConfig.class);
+
+        public CorsFilter corsFilter() {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080",
+                                "http://159.54.139.5:81", "http://159.54.139.5:80"));
+                config.setAllowedMethods(List.of("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+                config.setAllowedOrigins(Collections.singletonList("*"));
+                config.addAllowedHeader("*");
+                config.addExposedHeader("location");
+                config.setAllowCredentials(true);
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", config);
+                CorsFilter filter = new CorsFilter(source);
+                return filter;
+        }
 
 }
