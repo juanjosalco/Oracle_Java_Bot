4a24e450831f21d9e1dfe3b6b5629570d9f7a63a
Geek-Deer <geekdeer@gmail.com>
2024-05-22 09:29:38 -0600

    Modified the AXIOS URL, we are https now!

--- backend/src/orabot/src/api/config/AxiosConfig.js
+++ backend/src/orabot/src/api/config/AxiosConfig.js
@@ -1,5 +1,5 @@
 export const axiosConfig = {
-    baseURL: "http://159.54.139.5:8483",
+    baseURL: "https://team16.kenscourses.com:8483",
     headers: {
         "Content-Type": "application/json",
     }
