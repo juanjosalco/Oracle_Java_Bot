75e1e0ea6452fbdc4ef3653d0ce8f4f5ae93034a
Jesus Enrique Diaz <a00227255@tec.mx>
2024-05-24 14:18:40 -0600

    Bot now working on Telegram with backend

--- backend/src/main/java/com/talentpentagon/javabot/JavabotApplication.java
+++ backend/src/main/java/com/talentpentagon/javabot/JavabotApplication.java
@@ -1,13 +1,32 @@
 package com.talentpentagon.javabot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import com.talentpentagon.javabot.bot.OraBot;
 
 @SpringBootApplication
-public class JavabotApplication {
+public class JavabotApplication implements CommandLineRunner{
+
+	private String telegramBotToken = "7148391306:AAG0LN85Z9pKee3DgoevkzzfJEYs-2-cb9A";
+
+	private String botName = "TC3005OraBot";
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavabotApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		try {
+			TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+			telegramBotsApi.registerBot(new OraBot(telegramBotToken, botName));
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ backend/src/main/java/com/talentpentagon/javabot/bot/OraBot.java
@@ -0,0 +1,54 @@
+package com.talentpentagon.javabot.bot;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.api.objects.webapp.WebAppInfo;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class OraBot extends TelegramLongPollingBot {
+    private String botName;
+    private String botToken;
+    
+    public OraBot(String botToken, String botName) {
+        super(botToken);
+        this.botName = botName;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        SendMessage message = new SendMessage();
+        message.setChatId(update.getMessage().getChatId());
+        message.setText("Welcome!");
+
+
+        KeyboardButton button = new KeyboardButton("WebApp");
+        button.setWebApp(new WebAppInfo("https://team16.kenscourses.com"));
+
+        
+        // ReplyKeyboardMarkup markupInline = new ReplyKeyboardMarkup();
+        // markupInline.setKeyboard(null);
+        // @NonNull List<KeyboardRow> rowsInline = new ArrayList<>();
+
+        // markupInline.setKeyboard(rowsInline);
+
+        // message.setReplyMarkup(markupInline);
+        
+        try {
+            execute(message); // Sends the message with the inline keyboard
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
