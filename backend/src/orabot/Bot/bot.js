893d8d81f327882983e69abfb30d8555a3997b62
Jesus Enrique Diaz <a00227255@tec.mx>
2024-05-24 14:18:40 -0600

    Bot now working on Telegram with backend

--- backend/src/orabot/Bot/bot.js
+++ backend/src/orabot/Bot/bot.js
@@ -1,7 +1,7 @@
 const {Telegraf} = require('telegraf')
 
 const TOKEN = "7148391306:AAG0LN85Z9pKee3DgoevkzzfJEYs-2-cb9A"
-const WEBSITE = "https://orabot.netlify.app/"
+const WEBSITE = "https://team16.kenscourses.com/"
 
 const bot = new Telegraf(TOKEN)
 bot.start((ctx) => ctx.reply('Welcome', {reply_markup: {keyboard: [[{text: 'Web App', web_app: {url: WEBSITE}}]]} }))
