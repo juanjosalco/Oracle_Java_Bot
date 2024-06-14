96b3b36fab2fbf155988b9851f58d9fb999eb809
Geek-Deer <geekdeer@gmail.com>
2024-06-13 23:40:08 -0600

    Changed baseURL... again

--- backend/src/orabot/src/api/config/AxiosConfig.js
+++ backend/src/orabot/src/api/config/AxiosConfig.js
@@ -1,6 +1,6 @@
 export const axiosConfig = {
-    // baseURL: "https://team16.kenscourses.com:8483",
-    baseURL: "http://localhost:8080",
+    baseURL: "https://team16.kenscourses.com:8483",
+    // baseURL: "http://localhost:8080",
     headers: {
         "Content-Type": "application/json",
     }
