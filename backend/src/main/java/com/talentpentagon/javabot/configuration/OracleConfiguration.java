62a85c704b0583d1e4e5f55da75c840e8fff9824
Geek-Deer <geekdeer@gmail.com>
2024-06-13 18:49:58 -0600

    Updated to cloud config

--- backend/src/main/java/com/talentpentagon/javabot/configuration/OracleConfiguration.java
+++ backend/src/main/java/com/talentpentagon/javabot/configuration/OracleConfiguration.java
@@ -1,43 +1,43 @@
-// package com.talentpentagon.javabot.configuration;
+package com.talentpentagon.javabot.configuration;
 
-// import oracle.jdbc.pool.OracleDataSource;
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.core.env.Environment;
+import oracle.jdbc.pool.OracleDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
-// import javax.sql.DataSource;
-// import java.sql.SQLException;
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 
-// @Configuration
-// public class OracleConfiguration {
-// Logger logger = LoggerFactory.getLogger(DbSettings.class);
-// // @Autowired
-// // private DbSettings dbSettings;
+@Configuration
+public class OracleConfiguration {
+Logger logger = LoggerFactory.getLogger(DbSettings.class);
 // @Autowired
-// private Environment env;
-// @Bean
-// public DataSource dataSource() throws SQLException{
-//     OracleDataSource ds = new OracleDataSource();
-//     ds.setDriverType(env.getProperty("driver_class_name"));
-//     logger.info("Using Driver " + env.getProperty("driver_class_name"));
-//     ds.setURL(env.getProperty("db_url"));
-//     logger.info("Using URL: " + env.getProperty("db_url"));
-//     ds.setUser(env.getProperty("db_user"));
-//     logger.info("Using Username " + env.getProperty("db_user"));
-//     ds.setPassword(env.getProperty("dbpassword"));
+// private DbSettings dbSettings;
+@Autowired
+private Environment env;
+@Bean
+public DataSource dataSource() throws SQLException{
+    OracleDataSource ds = new OracleDataSource();
+    ds.setDriverType(env.getProperty("driver_class_name"));
+    logger.info("Using Driver " + env.getProperty("driver_class_name"));
+    ds.setURL(env.getProperty("db_url"));
+    logger.info("Using URL: " + env.getProperty("db_url"));
+    ds.setUser(env.getProperty("db_user"));
+    logger.info("Using Username " + env.getProperty("db_user"));
+    ds.setPassword(env.getProperty("dbpassword"));
 
-//     // For local testing
-//     // ds.setDriverType(dbSettings.getDriver_class_name());
-//     // logger.info("Using Driver " + dbSettings.getDriver_class_name());
-//     // ds.setURL(dbSettings.getUrl());
-//     // logger.info("Using URL: " + dbSettings.getUrl());
-//     // ds.setUser(dbSettings.getUsername());
-//     // logger.info("Using Username: " + dbSettings.getUsername());
-//     // ds.setPassword(dbSettings.getPassword());
-//     return ds;
-//     }
-// }
+    // For local testing
+    // ds.setDriverType(dbSettings.getDriver_class_name());
+    // logger.info("Using Driver " + dbSettings.getDriver_class_name());
+    // ds.setURL(dbSettings.getUrl());
+    // logger.info("Using URL: " + dbSettings.getUrl());
+    // ds.setUser(dbSettings.getUsername());
+    // logger.info("Using Username: " + dbSettings.getUsername());
+    // ds.setPassword(dbSettings.getPassword());
+    return ds;
+    }
+}
